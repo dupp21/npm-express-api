8feaab4640e952c7b30cb07e8cb1375036985240
Domingos Bento Pereira <luakay@gmail.com>
2018-11-06 15:52:00 +0700

    Learn Express NPM

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let todoList = [
+  {
+    description: "Learn React",
+    done: false
+  },
+  {
+    description: "learn Javascript",
+    done: false
+  }
+];
+
+// Api Info
+app.get("/", (req, res) => {
+  res.send("This is api demo");
+});
+
+// Read
+app.get("/todos", (req, res) => {
+  res.send("todos");
+});
+
+// Create
+app.post("/todos", (req, res) => {
+  todoList.push(req.body);
+  res.send(req.body);
+});
+
+// Search
+app.get("/todos/search", (req, res) => {
+  const result = todoList.filter((todo, index) => {
+    return (
+      todo.description === req.query.description ||
+      todo.done === JSON.parse(req.query.done)
+    );
+  });
+  res.send(result);
+});
+
+//Read One Data
+app.get("/todos/:id", (req, res) => {
+  res.send(todoList[req.params.id]);
+});
+
+// Update
+app.put("/todos/:id", (req, res) => {
+  todoList[req.param.id] = req.body;
+  res.send(todoList);
+});
+
+// Delete One
+app.delete("/todos/:id", (req, res) => {
+  todoList.splice(req.param.id, 1);
+  res.send(`Delete todo with id`);
+});
+
+// Delete All
+app.delete("/todos/", (req, res) => {
+  todoList = [];
+  res.send(todoList);
+});
+
+app.listen(3000, () => console.log("application running on port 3000"));
